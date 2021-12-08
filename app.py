2bf5426392c85d659cac9b7033e2c0c74e86cf8d
Ahmad Ahmed <zcaptain2002@gmail.com>
2021-12-08 22:12:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import xlrd
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import LSTM
